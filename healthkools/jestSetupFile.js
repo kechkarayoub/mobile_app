10e92588afb267bf0e9853bf13d209347d49fb0f
kechkarayoub <kechkarayoub@gmail.com>
2022-08-25 22:31:22 +0100

    begin config jest

--- healthkools/jestSetupFile.js
+++ healthkools/jestSetupFile.js
@@ -1,3 +1,7 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+// jest.mock('@react-navigation/native/lib/commonjs/useLinking.native', () => ({
+//   default: () => ({getInitialState: {then: jest.fn()}}),
+//   __esModule: true,
+// }));
